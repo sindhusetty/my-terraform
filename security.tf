f37fd850f719ffd9a41d44f89fc592130fedb71c
sindhusetty <93355932+sindhusetty@users.noreply.github.com>
2021-10-28 20:15:31 -0400

    Add files via upload

---
+++ security.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "sg_oregon" {
+  name        = "tf-sg"
+  description = "Allow web traffics"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress = [
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    },
+
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    }
+  ]
+
+
+  egress = [
+    {
+      description      = "for all outgoing traffics"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    }
+  ]
+
+  tags = {
+    Name = "sg-for-subnet"
+  }
+}
